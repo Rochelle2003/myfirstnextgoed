49e45bb6962f04d86a68453bf2ff967f1bb37e60
Rochelle2003 <rochellemannie2003@outlook.com>
2025-08-22 14:19:56 +0200

    pages fix
    paginas werkend gemaakt na terugkomende error

--- app/Lib/supabaseClient.js
+++ app/Lib/supabaseClient.js
@@ -4,3 +4,4 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://mempjlccgmj
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lbXBqbGNjZ21qZnl0cnBrdW5rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1MTMzMDUsImV4cCI6MjA3MTA4OTMwNX0.hYuKyqfEcmsTU5qenwDysuQb4dxQtisGGUO34qhZUys'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
--- app/auth/register/page.jsx
+++ app/auth/register/page.jsx
@@ -146,3 +146,4 @@ export default function Register() {
     </div>
   );
 }
+
